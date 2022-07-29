e3a5dcf982d46d73474af1f90c7cbd608513aa61
MBenmasour95 <mehdi.benmansour95@gmail.com>
2022-07-29 12:21:26 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+/* --- DOM Element --- */
+const navbar = document.getElementById("navbar");
+
+/* --- EventListener --- */
+navbar.addEventListener("click", handleClink);
+document.addEventListener("scroll", () => {
+  const navItems = document.querySelector(".nav-items");
+
+  navItems.style.top = `${this.scrollY}px`;
+});
+
+/* --- Function --- */
+function handleClink(e) {
+  const target = e.target;
+
+  if (target.classList.contains("drop-btn")) {
+    target.classList.toggle("clicked");
+    target.nextElementSibling.classList.toggle("dropped");
+  } else if (target.classList.contains("menu-btn")) {
+    target.parentElement.querySelector(".nav-items").classList.add("fadeIn");
+    document.body.style.overflow = "hidden";
+  } else if (target.classList.contains("close-btn")) {
+    target
+      .closest(".navbar")
+      .querySelector(".nav-items")
+      .classList.remove("fadeIn");
+    document.body.style.overflow = "visible";
+  }
+}
